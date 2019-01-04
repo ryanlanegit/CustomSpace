1d23f0b3166ad32ea5410d4a0957a8cfcda17148
Ryan Lane <github@ryan-lane.com>
2019-01-04 11:50:56 -0800

    Update build.bat

--- build/build.bat
+++ build/build.bat
@@ -8,35 +8,35 @@ fltmc >nul 2>&1 && (
     ECHO ************************************************ >> built.log
     ECHO Building \custom.css >> built.log & cls & type built.log
     node r.js -o build-customCSS.js >> built.log & cls & type built.log
-    
+
     ECHO ************************************************ >> built.log
     ECHO Building \Scripts\viewMain.js >> built.log & cls & type built.log
     node r.js -o build-viewMain.js >> built.log & cls & type built.log
-    
+
     ECHO ************************************************ >> built.log
     ECHO Building \Scripts\forms\profileMain.js >> built.log & cls & type built.log
     node r.js -o build-profileMain.js >> built.log & cls & type built.log
-    
+
     ECHO ************************************************ >> built.log
     ECHO Building \Scripts\forms\wiMain.js >> built.log & cls & type built.log
     node r.js -o build-wiMain.js >> built.log & cls & type built.log
-    
+
     ECHO ************************************************ >> built.log
     ECHO Building \Scripts\forms\wiActivityMain.js >> built.log & cls & type built.log
     node r.js -o build-wiActivityMain.js >> built.log & cls & type built.log
-    
+
     ECHO ************************************************ >> built.log
     ECHO Building \CustomSpace\Scripts\serviceCatalog\roTaskMain.js >> built.log & cls & type built.log
     node r.js -o build-roTaskMain.js >> built.log & cls & type built.log
-    
+
     ECHO ************************************************ >> built.log
     ECHO Building \CustomSpace\Scripts\forms\wiTaskMain.js >> built.log & cls & type built.log
     node r.js -o build-wiTaskMain.js >> built.log & cls & type built.log
-    
+
     ECHO ************************************************ >> built.log
     ECHO Building \CustomSpace\Scripts\grids\gridTaskMain.js >> built.log & cls & type built.log
     node r.js -o build-gridTaskMain.js >> built.log & cls & type built.log
-    
+
     ECHO ************************************************ >> built.log
     ECHO Building \CustomSpace\Scripts\page\pageTaskMain.js >> built.log & cls & type built.log
     node r.js -o build-pageTaskMain.js >> built.log & cls & type built.log
