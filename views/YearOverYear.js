d1226b65e46d60f4dea5e5df70039e0fc5fdfc32
Ryan Lane <github@ryan-lane.com>
2019-02-07 21:55:45 -0800

    Adding year over year

---
+++ views/YearOverYear.js
@@ -0,0 +1,33 @@
+﻿{
+    "Id": "YearOverYear",
+    "layoutType": "semantic",
+	"pageTitle": "Operations Year Over Year",
+    "view": {
+        "cssClass": "container-fluid",
+		"content": {
+			"cssClass": "row",
+			"content": [
+				{
+					"cssClass": "col-sm-12",
+					"content": {
+						"cssClass": "row dashboard-container",
+						"content" : [
+							{
+								"cssClass": "col-sm-12",
+								"type": "viewPanel",
+								"ViewPanelId": "vp-YearOverYear-Title",
+								"title": "Operations Year Over Year TEST",
+								"subTitle": "Test"
+							},
+							{
+								"cssClass": "col-sm-12",
+								"type": "viewPanel",
+								"ViewPanelId": "vp-YearOverYear"
+							}
+						]
+					}
+				}
+			]
+		}
+    }
+}
---
+++ views/viewpanels/vp-YearOverYear-Title.js
@@ -0,0 +1,7 @@
+{
+    "Id": "vp-YearOverYear-Title",
+    "TypeId": "html",
+    "Definition" : {
+		"content": "<h1 class='page_title'>Operations - Year Over Year</h1><p class='text-muted'></p>"
+	}
+}
