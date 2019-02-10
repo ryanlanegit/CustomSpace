fa3ced5be3c5c76e7a48d7ed13c9b26f60f2c6e2
Ryan Lane <github@ryan-lane.com>
2019-02-09 17:33:13 -0800

    Updating AMDClean function to fix Source Map references

--- build/build-wiTaskMain.js
+++ build/build-wiTaskMain.js
@@ -24,15 +24,27 @@
   preserveLicenseComments: false,
   onModuleBundleComplete: function (data) {
     'use strict';
-    console.log('AMD Cleaning File:' + data.path);
+    console.log('AMD Cleaning File: ' + data.path);
     var fs = module.require('fs'),
-      // AMDClean Module from https://github.com/gfranko/amdclean
+        // AMDClean Module from https://github.com/gfranko/amdclean
         amdclean = module.require('amdclean'),
         inputFile = data.path,
-        outputFile = './build/clean.js',
-        cleanedCode = amdclean.clean({
-          'filePath': inputFile,
+        inputSourceMap = inputFile + '.map',
+        sourceMapContents = fs.readFileSync(inputSourceMap, 'utf8').replace(/[\r\n]+/g, ''),
+        cleaned = amdclean.clean({
+          filePath: inputFile,
+          sourceMap: sourceMapContents,
+          wrap: false,
+          esprima: {
+              loc: true,
+              source: 'wiTaskMain.js',
+          },
+          escodegen: {
+            sourceMap: true,
+            sourceMapWithCode: true,
+          },
         });
-    fs.writeFileSync(inputFile, cleanedCode);
+    fs.writeFileSync(inputFile, cleaned.code);
+    fs.writeFileSync(inputSourceMap, cleaned.map);
   },
 })
